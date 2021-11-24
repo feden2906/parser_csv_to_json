cc16906ca4563226f16138f756ef914d87ae839a
feden2906 <feden2906@gmail.com>
2021-11-24 23:29:31 +0200

    working for one input file

--- index.js
+++ index.js
@@ -11,38 +11,43 @@ const dateBuilder = (date) => date
   .join('-');
 
 const dataNormalizer = (item) => {
-  console.log(item);
   const {
     first_name, last_name, cc, amount, date
   } = item;
 
-  // const a = {
-  //   name: `${last_name} ${first_name}`,
-  //   phone: String,
-  //   person: {
-  //     firstName: first_name,
-  //     lastName: last_name
-  //   },
-  //   amount,
-  //   date: dateBuilder(date),
-  //   costCenterNum: cc.slice(3)
-  // };
-// return a;
+  const a = {
+    name: `${last_name} ${first_name}`,
+    phone: String,
+    person: {
+      firstName: first_name,
+      lastName: last_name
+    },
+    amount,
+    date: dateBuilder(date),
+    costCenterNum: cc.slice(3)
+  };
+return a;
 };
 
-const readStream = fs.createReadStream('./input_data/users1.csv');
+const readStream = fs.createReadStream('./input_data/users2.csv');
 const writeStream = fs.createWriteStream('./output_data/users.json');
 
 readStream.pipe(csv({ delimiter: '||', toArrayString: true, downstreamFormat: 'array' }))
   .on('data', (chunk) => {
-    // const a = dataNormalizer(chunk.toString());
-    // const b = JSON.stringify(a);
-    console.log('==============================================================')
-    console.log(chunk.toString().length)
-    console.log('==============================================================')
-    writeStream.write(chunk);
+    if (chunk.length <= 2) {
+      writeStream.write(chunk);
+      return;
+    }
+
+    chunk = chunk.toString();
+    chunk = chunk.slice(0, chunk.length - 2);
+    chunk = JSON.parse(chunk);
+    console.log(chunk);
+    const a = dataNormalizer(chunk);
+
+    console.log(a)
+    writeStream.write(`${JSON.stringify(a)},`);
   })
-  // .pipe(writeStream)
   .on('end', () => {
     console.log('Finish');
   }).then(() => {});
